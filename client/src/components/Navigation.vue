12a92454d732014590c145dad8d32707c0658423
nicolaeceabin <kolia.ceabin@gmail.com>
2022-03-18 15:29:13 +0100

    zmena

---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="navbar bg-dark navbar-expand-lg navbar-dark fixed-top " id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <router-link class="navbar-brand text-center$ git config --global user.name " to="/">My Awesome Blog</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <em class="bi-list"/>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link px-lg-1 py-3 py-lg-4" to="/">
+                            <em class="bi-house"/>
+                            Home
+                        </router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link px-lg-1 py-3 py-lg-4" to="/about">
+                            <em class="bi-newspaper"/>
+                            About
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link px-lg-1 py-3 py-lg-4" to="/new-article">
+                            <em class="bi-plus-circle"/>
+                            NEW
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navigation"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<script>
+import Article from "@/components/Article.vue";
+import Preview from "@/components/Preview.vue";
+
+export default {
+    components: {Preview, Article},
+    data() {
+        return {
+            articles: [
+                {
+                    image: 'https://www.letemsvetemapplem.eu/wp-content/uploads/2022/03/Mac-Studio-Studio-Display-3-1536x1023.jpg.webp',
+                    title: "Mac Studio",
+                    date: new Date(),
+                    text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
+Integer tempor. Pellentesque ipsum. Integer malesuada. Vestibulum fermentum tortor id mi. Aenean placerat.
+Pellentesque arcu. Phasellus rhoncus.
+`
+                },
+                {
+                    image: 'https://samsungmagazine.eu/wp-content/uploads/2022/01/hacker-ga09d64f38_1920-Large-255x196.jpeg.webp',
+                    title: "Chyba v Androidu 12 umožňuje hacknout některé smartphony",
+                    date: new Date(),
+                    text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
+Integer tempor. Pellentesque ipsum. Integer malesuada. Vestibulum fermentum tortor id mi. Aenean placerat.
+Pellentesque arcu. Phasellus rhoncus.
+`
+                },
+                {
+                    image: 'https://samsungmagazine.eu/wp-content/uploads/2017/02/Instagram-FB.png.webp',
+                    title: "Instagram ukončuje další své samostatné aplikace",
+                    date: new Date(),
+                    text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
+Integer tempor. Pellentesque ipsum. Integer malesuada. Vestibulum fermentum tortor id mi. Aenean placerat.
+Pellentesque arcu. Phasellus rhoncus.
+`
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2  pb-3 pt-3" v-for="article in articles">
+            <Preview :title="article.title" :date="article.date" :text="article.text" :image="article.image"></Preview>
+        </div>
+    </div>
+</template>
