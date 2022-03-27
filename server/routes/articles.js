f3c2104e1f3f108a4798ee6d30989e17f5fa541b
nicolaeceabin <kolia.ceabin@gmail.com>
2022-03-27 16:45:03 +0200

    zmena

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -1,36 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-const articles = [
+let articles = [
     {
         id: 1,
         image: 'https://picsum.photos/seed/picsum/500',
         title: "Title",
         date: new Date(),
-        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
-Integer tempor. Pellentesque ipsum. Integer malesuada. Vestibulum fermentum tortor id mi. Aenean placerat.
-Pellentesque arcu. Phasellus rhoncus.
-`
+        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.`
     },
     {
         id: 2,
         image: 'https://picsum.photos/seed/picsum/500',
         title: "Title",
         date: new Date(),
-        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
-Integer tempor. Pellentesque ipsum. Integer malesuada. Vestibulum fermentum tortor id mi. Aenean placerat.
-Pellentesque arcu. Phasellus rhoncus.
-`
+        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.`
     },
     {
         id: 3,
         image: 'https://picsum.photos/seed/picsum/500',
         title: "Title",
         date: new Date(),
-        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.
-Integer tempor. Pellentesque ipsum. Integer malesuada. Vestibulum fermentum tortor id mi. Aenean placerat.
-Pellentesque arcu. Phasellus rhoncus.
-`
+        text: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam egestas wisi a erat.`
     },
 ];
 router.get("/", function (req, res, next) {
@@ -50,15 +41,47 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
     const body =req.body;
     const article ={
-        id:aricles.count,
+        id:articles.length,
         title: body.title,
         text:body.text,
-        date: new date()
+        date: new Date()
     }
     articles.push(article);
-    console.table(aricles);
+    console.table(articles);
     console.debug(body);
     res.send({});
     });
+router.patch("/:id",(req, res)=>{
+  const body =req.body;
+  const id= req.params.id;
+    if (id) {
+        const article = articles.find((a) => a.id === Number.parseInt(id));
+        if(article){
+            article.title = body.title;
+            article.text = body.text;
+            res.send(article);
+        }
+        else{
+            res.sendStatus(404)
+        }
+        res.send(article);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+router.delete("/:id",(req, res)=>{
+    const id= req.params.id;
+    if (id) {
+         articles = articles.filter((a) => a.id !xx== Number.parseInt(id));
+        res.sendStatus(200);
+
+            res.send(article);
+
+
+        } else {
+        res.sendStatus(404);
+    }
+});
 
 module.exports = router;
